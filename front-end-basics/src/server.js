f345c197c6c96ded2d7a214eede955f90a609746
Dmitriy Yermakov <dmitriy_xix@hotmail.com>
2025-12-10 04:19:44 -0500

    nodejs-test

---
+++ front-end-basics/src/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/api/users", (req, res) => {
+  const users = [
+    {
+      id: "1",
+      name: "shaun",
+    },
+    {
+      id: "2",
+      name: "joe",
+    },
+    {
+      id: "3",
+      name: "zeus",
+    },
+  ];
+  res.json(users);
+});
+
+app.listen(8080, () => {
+  console.log("server is listening on port 8080");
+});
